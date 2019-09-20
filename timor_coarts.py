8a89333c64234075a01de9efeb6782438e55cd0d
griCe14807 <36400250+griCe14807@users.noreply.github.com>
2019-09-20 17:21:34 +0900

    Initial commit

---
+++ timor_coarts.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+import matplotlib as plt
+
+
+# ---------inputs-------------------
+
+input_folderpath = r"C:\Users\02217013\Documents\201907-_DOMEE\PPRT勉強会\02_Data\nmr"
+input_filename = "Calliance1.csv"
+
+# ---------------------------------
+
+input_file = os.path.join(input_folderpath, input_filename)
+print(input_file)
+Calliance1_nmr_df = pd.read_csv(input_file)
+print(Calliance1_nmr_df.head)
+
+# T2 distributionの表示
