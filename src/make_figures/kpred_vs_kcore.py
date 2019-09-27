39ddc1738a25ed620821d614e32f76aa4d881a6b
griCe14807 <36400250+griCe14807@users.noreply.github.com>
2019-09-27 13:35:59 +0900

    aa

--- src/make_figures/kpred_vs_kcore.py
+++ src/make_figures/kpred_vs_kcore.py
@@ -44,20 +44,20 @@ if __name__ == "__main__":
     # ---------inputs-------------------
 
     input_folderpath = r"D:\PPRT関係\data"
-    input_filename = "Calliance1.csv"
+    input_filename_list = ["Calliance1.csv", "Brecknock4_nmr.csv", "Noblige2_nmr.csv"]
 
     # output_file = r"D:\PPRT関係\result\Caliance1_ktim.csv"
 
     # ---------------------------------
 
     # csvファイル読み込み
-    input_file = os.path.join(input_folderpath, input_filename)
-    Calliance1_nmr_df = pd.read_csv(input_file)
+    the_input_df = pd.concat(pd.read_csv(os.path.join(input_folderpath, input_filename)) for input_filename in input_filename_list)
 
     # c_coatesを変えると、プロット上ではどんな変化をするのか（平行移動？）確認.
     # confirm_c_coates(Calliance1_nmr_df)
 
     # cut_coatesをしていしたverと指定しないverでそれぞれcperm vs ktimのプロットを作成し、どの程度それっぽさがアップするかを確認
+
     # グラフをみてじぶんで定義したcut_coates
     the_cut_coates_dict = {"Calliance1_22": 15,
                            "Calliance1_24": 20,
@@ -67,20 +67,20 @@ if __name__ == "__main__":
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
 
-    Calliance1_nmr_result_df_dict = {}
+    the_nmr_result_df_dict = {}
     # default cut_coates
-    Calliance1_nmr_result_df_dict["default_cut"] = ktim.main(Calliance1_nmr_df)
+    the_nmr_result_df_dict["default_cut"] = ktim.main(the_input_df)
     # my defined cut coates
-    Calliance1_nmr_result_df_dict["my_cut"] = ktim.main(Calliance1_nmr_df, cut_coates_dict=the_cut_coates_dict)
+    the_nmr_result_df_dict["my_cut"] = ktim.main(the_input_df, cut_coates_dict=the_cut_coates_dict)
 
-    for key, Calliance1_result_df in Calliance1_nmr_result_df_dict.items():
-        print(key, Calliance1_result_df)
+    for key, the_result_df in the_nmr_result_df_dict.items():
+        print(key, the_result_df)
 
-        x = Calliance1_result_df["ktim"]
-        y = Calliance1_result_df["cperm"]
+        x = the_result_df["ktim"]
+        y = the_result_df["cperm"]
 
         ax.scatter(x, y, label=key)
-        for i, row in Calliance1_result_df.iterrows():
+        for i, row in the_result_df.iterrows():
             ax.annotate(row["ID"], (row["ktim"], row["cperm"]), size=8)
 
     ax.set_xscale("log")
@@ -91,4 +91,3 @@ if __name__ == "__main__":
     ax.set_ylabel('perm_core')
     ax.legend()
     plt.show()
-
