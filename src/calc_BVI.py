129db37123cf75042e3cc2bb285203cd3dc7c192
griCe14807 <36400250+griCe14807@users.noreply.github.com>
2019-11-20 15:17:20 +0900

    Update calc_BVI.py

--- src/calc_BVI.py
+++ src/calc_BVI.py
@@ -110,10 +110,10 @@ def BVI_spectral_empirical(df, m, b):
     df["Swi"] = 1 / (m * df.T2.copy() + b)
     
     # mについて微分した式
-    df["Swi_dm"] = - df.T2.copy * ((m * df.T2.copy + b) ** (-2))
+    df["Swi_dm"] = -1 * (df.T2.copy() * ((m * df.T2.copy() + b) ** (-2)))
     
     # bについて微分した式
-    df["Swi_db"] = -((m * df.T2.copy + b) ** (-2))
+    df["Swi_db"] = -1 * ((m * df.T2.copy() + b) ** (-2))
     
     # T2 < T2i（すなわちWi > W) のところではSwi = 1, Swiの傾きは0
     df.Swi[df.Swi > 1] = 1
@@ -144,21 +144,54 @@ def m_b_spectral_empirical(df, m0, b0):
         key_1, group_1 = combi_group[0]
         key_2, group_2 = combi_group[1]
         
+        # 初期設定のmとb
+        m = m0
+        b = b0
+
         # objective_1とobjective_2の連立方程式をm, bについて解く
-        objective_1 = BVI_spectral_empirical(group_1, m, b)[0] - group_1.BVI_Por_cum.iloc[-1]
-        objective_2 = BVI_spectral_empirical(group_2, m, b)[0] - group_2.BVI_Por_cum.iloc[-1]
-        
-        objective_dm_1 = BVI_spectral_empirical(group_1, m, b)[1]
-        objective_db_1 = BVI_spectral_empirical(group_1, m, b)[1]
-        
-        objective_dm_2 = BVI_spectral_empirical(group_2, m, b)[2]
-        objective_db_2 = BVI_spectral_empirical(group_2, m, b)[2]
-        
-        # ニュートン法を用いてgroup1, group2についての最適なm, bの組み合わせを算出
+        # ニュートン法を用いる
         for count in range(1000):
             
+            # テイラー展開を一次の項で打ち切り、線形一次方程式で近似
+            objective_1 = BVI_spectral_empirical(group_1, m, b)[0] - group_1.BVI_Por_cum.iloc[-1]
+            objective_2 = BVI_spectral_empirical(group_2, m, b)[0] - group_2.BVI_Por_cum.iloc[-1]
+            
+            objective_dm_1 = BVI_spectral_empirical(group_1, m, b)[1]
+            objective_db_1 = BVI_spectral_empirical(group_1, m, b)[1]
+            
+            objective_dm_2 = BVI_spectral_empirical(group_2, m, b)[2]
+            objective_db_2 = BVI_spectral_empirical(group_2, m, b)[2]
+            
+            # 計算をしやすくするために行列の形にする
+            var_array = np.array([m, b])
+            
+            objective_array = np.array([objective_1, objective_2])
+            
+            gradient_matrix = np.array([[objective_dm_1, objective_db_1],
+                                        [objective_dm_2, objective_db_2]])
+            
+            # 漸化式を解き、次のステップのm, bを算出
+            next_var_array = var_array - np.dot(gradient_matrix.T, objective_array)
+            m = next_var_array[0]
+            b = next_var_array[1]
+            
+            diff = np.sqrt(np.sum((var_array - next_var_array) ** 2))
+            
+            if diff > 1:
+                # print("m={0}, b={1}, continue".format(m, b))
+                continue
+            else:
+                print("break! k1={0}, k2={1}, count={2}".format(key_1, key_2, count))
+                m_list.append(m)
+                b_list.append(b)
+                break
+    
+    print(m_list)
+    print(b_list)
         
     # m_listおよびb_listの平均値を最適なm, bの値とする
+    optimized_m = sum(m_list) / len(m_list)
+    optimized_b = sum(b_list) / len(b_list)
     
     return optimized_m, optimized_b
 
@@ -168,14 +201,17 @@ if __name__ == "__main__":
 
     # ---------inputs-------------------
 
-    input_folderpath = r"D:\tmp"
+    input_folderpath = r"C:\Users\02217013\Documents"
     input_filename = "calliance1_nmr.csv"
     
     # irreducible conditionのときのPc とりあえずPsiで構わん（というか統一してあれば単位何でもいい）
     the_Pci = 50
 
-    # newton法をすたーとするxの値
+    # newton法をすたーとするx, m, bの値
     x0 = 1000
+
+    m0 = 0.055
+    b0 = 1
     
     # default cutoff値
     C = 33
@@ -186,7 +222,7 @@ if __name__ == "__main__":
     the_df = pd.read_csv(input_file)
     # the_cut_coates_dict = main(the_df)
     
-    m_b_spectral_empirical(the_df)
+    optimized_m, optimized_b = m_b_spectral_empirical(the_df, m0, b0)
     
     """
     for key, group in the_df.groupby("ID"):
